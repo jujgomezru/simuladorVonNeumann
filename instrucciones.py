77c4e8f6f46372db899ccf0c00bced51a6a3f6e2
nicortesg <nicortesg@unal.edu.co>
2025-05-20 21:33:34 -0500

    Update instrucciones.py
    Added intructions up to inc

--- instrucciones.py
+++ instrucciones.py
@@ -24,6 +24,25 @@ class Instrucciones:
             case 0xED: self.jnn(constante)
             case 0xEE: self.jnz(constante)
             case 0xFF: self.halt()
+            case 0x11: self.and_op(r1, r2, constante, modo)
+            case 0x13: self.or_op(r1, r2, constante, modo)
+            case 0x12: self.xor_op(r1, r2, constante, modo)
+            case 0x10: self.not_op(r1)
+            case 0x21: self.test(r1, r2)
+            case 0x90: self.input(r1, constante)
+            case 0x91: self.output(r1, constante)
+            case 0xD0: self.push(r1)
+            case 0xD1: self.pop(r1)
+            case 0xD8: self.call(constante)
+            case 0xD9: self.ret()
+            case 0x28: self.shl(r1, r2, constante)
+            case 0x29: self.shr(r1, r2, constante)
+            case 0x48: self.inc(r1)
+            case 0x49: self.dec(r1)
+            case 0xD2: self.load_sp(r1)
+            case 0xD3: self.store_sp(r1, constante)
+            case 0xF0: self.interrupt()
+            case 0xF1: self.return_interrupt()
             case _: print(f"Instrucción no implementada: {hex(opcode)}"); self.cpu.running = False
 
     def nop(self):
@@ -95,6 +114,67 @@ class Instrucciones:
     def jnn(self, dest):
         if self.cpu.FLAGS['N'] == 0:
             self.cpu.PC = dest
+
+    def and_op(self, r1, r2, constante, modo):
+        val = constante if modo == 0 else self.cpu.reg[r2]
+        res = self.cpu.reg[r1] & val
+        self.cpu.reg[r1] = res
+        self.set_flags(res)
+
+    def or_op(self, r1, r2, constante, modo):
+        val = constante if modo == 0 else self.cpu.reg[r2]
+        res = self.cpu.reg[r1] | val
+        self.cpu.reg[r1] = res
+        self.set_flags(res)
+
+    def xor_op(self, r1, r2, constante, modo):
+        val = constante if modo == 0 else self.cpu.reg[r2]
+        res = self.cpu.reg[r1] ^ val
+        self.cpu.reg[r1] = res
+        self.set_flags(res)
+
+    def not_op(self, r1):
+        res = (~self.cpu.reg[r1]) & self.MASK64
+        self.cpu.reg[r1] = res
+        self.set_flags(res)
+
+    def test(self, r1, r2):
+        res = self.cpu.reg[r1] & self.cpu.reg[r2]
+        self.set_flags(res)
+
+    def input(self, r1, constante):
+        val = int(input("Entrada (entero): "))
+        self.cpu.reg[r1] = val & self.MASK64
+
+    def output(self, r1, constante):
+        print(f"Salida R{r1}: {self.cpu.reg[r1]}")
+
+    def push(self, r1):
+        self.cpu.reg[15] = (self.cpu.reg[15] - 1) & self.MASK64
+        self.cpu.mem.escribir(self.cpu.reg[15], self.cpu.reg[r1])
+
+    def pop(self, r1):
+        self.cpu.reg[r1] = self.cpu.mem.leer(self.cpu.reg[15])
+        self.cpu.reg[15] = (self.cpu.reg[15] + 1) & self.MASK64
+
+    def call(self, constante):
+        self.cpu.reg[15] = (self.cpu.reg[15] - 1) & self.MASK64
+        self.cpu.mem.escribir(self.cpu.reg[15], self.cpu.PC)
+        self.cpu.PC = constante
+
+    def ret(self):
+        self.cpu.PC = self.cpu.mem.leer(self.cpu.reg[15])
+        self.cpu.reg[15] = (self.cpu.reg[15] + 1) & self.MASK64
+
+    def shl(self, r1, r2, constante):
+        res = (self.cpu.reg[r2] << constante) & self.MASK64
+        self.cpu.reg[r1] = res
+        self.set_flags(res)
+
+    def shr(self, r1, r2, constante):
+        res = (self.cpu.reg[r2] >> constante) & self.MASK64
+        self.cpu.reg[r1] = res
+        self.set_flags(res)
 
     def set_flags(self, result):
         self.cpu.FLAGS['Z'] = 1 if result == 0 else 0
